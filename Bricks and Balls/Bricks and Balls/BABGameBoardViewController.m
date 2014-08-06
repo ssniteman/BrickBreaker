3bbd8f3c503e936a07bb3e05b7b42f58aad0df79
Shane Sniteman <ssniteman@gmail.com>
2014-08-06 14:15:17 -0400

    moving from array, etc.

--- Bricks and Balls/Bricks and Balls/BABGameBoardViewController.m
+++ Bricks and Balls/Bricks and Balls/BABGameBoardViewController.m
@@ -57,8 +57,20 @@
         [animator addBehavior:gravityBehavior];
         
         collisionBehavior = [[UICollisionBehavior alloc] init];
-        collisionBehavior.translatesReferenceBoundsIntoBoundary = YES;
+//      collisionBehavior.translatesReferenceBoundsIntoBoundary = YES;
         collisionBehavior.collisionDelegate = self;
+        
+        // need to create boundaries so when ball hits bottom, it goes away
+        
+        [collisionBehavior addBoundaryWithIdentifier:@"floor" fromPoint:CGPointMake(0, SCREEN_HEIGHT) toPoint:CGPointMake(SCREEN_WIDTH, SCREEN_HEIGHT + 20)];
+        
+        [collisionBehavior addBoundaryWithIdentifier:@"left wall" fromPoint:CGPointMake(0,0) toPoint:CGPointMake(0, SCREEN_HEIGHT)];
+        
+        [collisionBehavior addBoundaryWithIdentifier:@"right wall" fromPoint:CGPointMake(SCREEN_WIDTH,0) toPoint:CGPointMake(SCREEN_WIDTH, SCREEN_HEIGHT)];
+        
+        [collisionBehavior addBoundaryWithIdentifier:@"ceiling wall" fromPoint:CGPointMake(0,0) toPoint:CGPointMake(SCREEN_WIDTH,0)];
+        
+        
         [animator addBehavior:collisionBehavior];
         
         brickItemBehavior = [[UIDynamicItemBehavior alloc] init];
@@ -76,7 +88,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 
-    paddle = [[UIView alloc] initWithFrame:CGRectMake((SCREEN_WIDTH - 100) / 2, SCREEN_HEIGHT - 20, 100, 4)];
+    paddle = [[UIView alloc] initWithFrame:CGRectMake((SCREEN_WIDTH - 100) / 2, SCREEN_HEIGHT - 10, 100, 4)];
     
     paddle.backgroundColor = [UIColor darkGrayColor];
     [self.view addSubview:paddle];
@@ -149,6 +161,17 @@
     
 }
 
+- (void)collisionBehavior:(UICollisionBehavior *)behavior beganContactForItem:(id<UIDynamicItem>)item withBoundaryIdentifier:(id<NSCopying>)identifier atPoint:(CGPoint)p
+{
+    if ([@"floor" isEqualToString:(NSString *)identifier])
+    {
+        UIView * ballItem = (UIView *)item;
+        [collisionBehavior removeItem:ballItem];
+        [ballItem removeFromSuperview];
+    }
+}
+
+
 
 - (void)collisionBehavior:(UICollisionBehavior *)behavior beganContactForItem:(id<UIDynamicItem>)item1 withItem:(id<UIDynamicItem>)item2 atPoint:(CGPoint)p
 {
@@ -158,6 +181,15 @@
         {
             [collisionBehavior removeItem:brick];
             [brick removeFromSuperview];
+            
+            [UIView animateWithDuration:0.3 animations:^{
+                brick.alpha = 0;
+            } completion:^(BOOL finished) {
+                [brick removeFromSuperview];
+                
+                [bricks removeObjectIdenticalTo:brick];
+            
+            }];
         }
     }
 }
@@ -179,6 +211,22 @@
 {
     UITouch * touch = [touches allObjects][0];
     CGPoint location = [touch locationInView:self.view];
+    
+    // fixes guard rail edge
+    
+    float guard = paddle.frame.size.width / 2.0 + 10;
+    float dragX = location.x;
+    
+    if (dragX < guard) dragX = guard;
+    if (dragX > SCREEN_WIDTH - guard) dragX = SCREEN_WIDTH - guard;
+    
+    
+    
+    
+    
+    
+    
+    attachmentBehavior.anchorPoint = CGPointMake(dragX, paddle.center.y);
     
     paddle.center = CGPointMake(location.x, paddle.center.y);
 
